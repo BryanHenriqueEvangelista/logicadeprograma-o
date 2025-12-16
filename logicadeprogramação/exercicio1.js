0e196896c47c5568b99bd7c4b7d808aec98c7ba1
BryanHenriqueEvangelista <bryanhenriquevangelista23@gmail.com>
2025-12-16 15:46:12 -0300

    adicionado comentários

--- logicadeprogramação/exercicio1.js
+++ logicadeprogramação/exercicio1.js
@@ -1,7 +1,8 @@
+// valores prédefinidos
 let v1 = 1;
 let v2 = 2;
 let v3 = 3;
-
+// função que calcula a media dos 3 valores
 function media(v1, v2, v3) {
   let resultado;
   resultado = (v1 + v2 + v3) / 3;
